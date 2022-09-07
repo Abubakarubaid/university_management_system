cd083d3319c68deda343d6a06d1a5ef73a55cc05
abubakarubaid <abubakarubaid.sgd@gmail.com>
2022-09-07 14:41:56 +0500

    Commit All Done

---
+++ build/disk_space/generated/source/buildConfig/release/de/appgewaltig/disk_space/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.appgewaltig.disk_space;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "de.appgewaltig.disk_space";
+  public static final String BUILD_TYPE = "release";
+}
